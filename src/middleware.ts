731b93172c92e8f8196fb8813326ddcccf2b9e04
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2024-02-29 12:00:31 +0500

    Fixing redirect issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
 export function middleware (req: NextRequest) {
-    const token = req.cookies.get('next-auth.session-token')
+    const token = req.cookies.get('__Secure-next-auth.session-token')
 
     if(!token) {
         return NextResponse.redirect(new URL('/login', req.url))
